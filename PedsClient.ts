a22c49cdf528bd0f2a883d6ee35e03a8e675144b
Rehabarians <32465229+Rehabarians@users.noreply.github.com>
2018-01-04 20:54:51 +0000

    Add files via upload

---
+++ PedsClient.ts
@@ -0,0 +1,38 @@
+﻿/// <reference path ="\types-gt-mp\Definitions\index.d.ts" />
+
+let PedHandle: LocalHandle;
+let PedPosition: Vector3;
+let PedRotation: Vector3;
+
+let VehicleHandle: LocalHandle;
+let VehiclePosition: Vector3;
+let VehicleRotation: Vector3;
+
+API.onResourceStart.connect(function () {
+
+});
+
+API.onServerEventTrigger.connect(function (event, args) {
+    if (event === "PedUpdate") {
+        //args[0] handle
+        //args[1] server position
+        //args[2] server rotation
+        PedHandle = args[0];
+        PedPosition = args[1];
+        PedRotation = args[2];
+    }
+    else if (event === "VehicleUpdate") {
+        //args[0] handle
+        //args[1] server position
+        //args[2] server rotation
+        VehicleHandle = args[0];
+        VehiclePosition = args[1];
+        VehicleRotation = args[2];
+    }
+});
+
+API.onEntityStreamIn.connect(function (EntityHandle, EntityType) {
+    if (EntityType === 8) {
+        
+    }
+});
---
+++ PedsServer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GrandTheftMultiplayer.Server;
+using GrandTheftMultiplayer.Server.API;
+using GrandTheftMultiplayer.Server.Constant;
+using GrandTheftMultiplayer.Server.Elements;
+using GrandTheftMultiplayer.Server.Extensions;
+using GrandTheftMultiplayer.Server.Managers;
+using GrandTheftMultiplayer.Server.Models;
+using GrandTheftMultiplayer.Server.Util;
+using GrandTheftMultiplayer.Shared;
+using GrandTheftMultiplayer.Shared.Gta;
+using GrandTheftMultiplayer.Shared.Math;
+
+namespace GTAPilotsPeds
+{
+    public class GTAPilotsPeds : Script
+    {
+        Ped Pilot;
+        Vehicle Luxor;
+
+        public GTAPilotsPeds()
+        {
+            API.onResourceStart += OnResourceStart;
+            API.onClientEventTrigger += OnClientEventTrigger;
+            API.onUpdate += OnUpdate;
+            //API.onPlayerFinishedDownload += OnPlayerFinishedDownload;
+        }
+
+        private void OnResourceStart()
+        {
+            Pilot = API.createPed(PedHash.Pilot01SMM, new Vector3(), 0);
+            Luxor = API.createVehicle(VehicleHash.Luxor, new Vector3(), new Vector3(), 0, 0);
+        }
+
+        private void OnClientEventTrigger(Client sender, string eventName, params object[] arguments)
+        {
+            if (eventName == "PedLocation")
+            {
+
+            }
+        }
+
+        private void OnUpdate()
+        {
+            API.triggerClientEventForAll("PedUpdate", Pilot.handle, Pilot.position, Pilot.rotation);
+            API.triggerClientEventForAll("VehicleUpdate", Luxor.handle, Luxor.position, Luxor.rotation);
+        }
+    }
+}
